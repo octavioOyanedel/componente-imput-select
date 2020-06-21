8acce702b4d30c7a3a656f7da539c240b4cc1775
Octavio Oyanedel <you@example.com>
2020-06-21 17:08:29 -0400

    Commit inicial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+    </head>
+    <body>
+        <h1>Formulario select 3 niveles</h1>
+        <h2>Provincia - Comuna - Cerro/Población/Villa</h2>
+    </body>
+</html>
